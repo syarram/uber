1f38dda0592118c51a58a035b116744088b0876d
Vincent LAURENT <vince.laurent@gmail.com>
2015-09-06 18:08:11 +0200

    Mise en conf des dossiers

--- archive.sh
+++ archive.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+. config.inc
+
 DATE=$1
-TMP_DIR=/tmp/uber_archive_$DATE
-TMP_JSON_DIR=$TMP_DIR/$DATE
-BASE_DIR=/home/vince/bin/uber_data
+TMP_UBER_DIR=/$TMP_DIR/uber_archive_$DATE
+TMP_UBER_JSON_DIR=$TMP_UBER_DIR/$DATE
 
 if ! test "$DATE"; then
     echo "La date est requise en premier paramètre"
@@ -15,4 +16,4 @@ if test -e $BASE_DIR/datas_archives/$DATE.tar.gz ; then
     exit;
 fi
 
-cd $BASE_DIR && mkdir -p $TMP_JSON_DIR && find datas/ -type f -name $DATE* | sort > $TMP_DIR/list && cat $TMP_DIR/list | sed 's/^/cp /' | sed "s|$| $TMP_JSON_DIR/|" > $TMP_DIR/archive.sh && bash $TMP_DIR/archive.sh && cd $TMP_DIR && tar -zcf $BASE_DIR/datas_archives/$DATE.tar.gz $DATE/ && cd $BASE_DIR && cat $TMP_DIR/list | sed 's/^/rm /' > $TMP_DIR/delete.sh && bash $TMP_DIR/delete.sh && rm -rf $TMP_DIR
+cd $BASE_DIR && mkdir -p $TMP_UBER_JSON_DIR && find datas/ -type f -name $DATE* | sort > $TMP_UBER_DIR/list && cat $TMP_UBER_DIR/list | sed 's/^/cp /' | sed "s|$| $TMP_UBER_JSON_DIR/|" > $TMP_UBER_DIR/archive.sh && bash $TMP_UBER_DIR/archive.sh && cd $TMP_UBER_DIR && tar -zcf $BASE_DIR/datas_archives/$DATE.tar.gz $DATE/ && cd $BASE_DIR && cat $TMP_UBER_DIR/list | sed 's/^/rm /' > $TMP_UBER_DIR/delete.sh && bash $TMP_UBER_DIR/delete.sh && rm -rf $TMP_UBER_DIR
